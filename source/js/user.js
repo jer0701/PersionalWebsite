ef0f9a5edc7856b4d90382469d6eba0f75c69645
jer0701 <1018995917@qq.com>
2017-12-27 17:30:23 +0800

    记住密码，采用base64对密码进行不可见编码

--- source/js/user.js
+++ source/js/user.js
@@ -1,10 +1,35 @@
-
-// 读取用户 cookie 显示
 (function(){
-  var $remember = $('#remember-me');
-  $('#login-submit').on('click', function(){
-    if($remember.checked){
-      Cookie.set('user-name', $('#email').val());
+
+
+  // 登录页
+  if($('#login-submit').length){
+    // 获取 cookies 并写入
+    var userName = Cookies.get('user-name');
+    var password = Cookies.get('password');
+        password = $.base64.atob(password);
+    if(userName){
+      $('#inputEmail').val(userName);
+      if(password){
+        $('#inputPassword').val(password);
+      }
     }
-  })
+
+
+    // 提交时重写 cookie
+    $('#login-submit').on('click', function(){
+      if($('#remember-me').prop('checked')){
+        var userName = $('#inputEmail').val();
+        var password = $('#inputPassword').val();
+        password = $.base64.btoa(password);
+        Cookies.set('user-name', userName, {path: '/admin', expires: 30 * 24 * 3600}); //记住30天 这是客户端存储的cookie
+        Cookies.set('password', password, {path: '/admin',expires: 30 * 24 * 3600}); //记住30天
+      } else {
+        Cookies.set('user-name', '', {path: '/admin', expires: -1000});
+        Cookies.set('password', '', {path: '/admin', expires: -1000});
+      }
+
+  });
+  }
+
+
 })();
