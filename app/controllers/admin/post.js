856aff6ea4121cf22ee3df939c696937c8f8b858
jer0701 <1018995917@qq.com>
2018-01-03 18:00:55 +0800

    撰写文章页面

--- app/controllers/admin/post.js
+++ app/controllers/admin/post.js
@@ -14,3 +14,14 @@ router.get('/', auth.isAuthenticated, function (req, res, next) {
 		title: '文章 - 博客管理系统'
 	});
 });
+router.get('/add', auth.isAuthenticated, function (req, res, next) {
+	res.render('admin/post/add', {
+		title: '撰写文章 - 博客管理系统',
+		action: '/admin/posts/add',
+		post: {
+			title: '',
+			category: {_id: ''}
+		},
+		edit: false
+	});
+});
